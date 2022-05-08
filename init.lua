89c506951cda1edc7e5664a5ad0b42617f85d459
3n3a <46775561+3n3a@users.noreply.github.com>
2022-05-08 21:28:12 +0200

    add packages for vim-plug

--- init.lua
+++ init.lua
@@ -1,3 +1,36 @@
+-- Installs Plugins with vim-plug
+
+local Plug = vim.fn['plug#']
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug('rcarriga/nvim-notify', {as='notify'})
+Plug('dracula/vim', { as = 'dracula' })
+Plug('kyazdani42/nvim-web-devicons', {as = 'nvim-web-devicons'})
+Plug('kyazdani42/nvim-tree.lua', {as='nvim-tree'})
+Plug('hrsh7th/nvim-compe', {as='compe'})
+Plug('akinsho/bufferline.nvim', {as='bufferline'})
+Plug('nvim-lua/plenary.nvim', {as='plenary'})
+Plug('TimUntersberger/neogit', {as='neogit'})
+Plug('lewis6991/impatient.nvim', {as='impatient'})
+Plug('neovim/nvim-lspconfig', {as='lspconfig'})
+Plug('glepnir/lspsaga.nvim', {as='lspsaga'})
+Plug('williamboman/nvim-lsp-installer', {as='nvim-lsp-installer'})
+Plug('andweeb/presence.nvim', {as='presence'})
+Plug('nvim-telescope/telescope.nvim', {as='telescope'})
+Plug('nvim-treesitter/nvim-treesitter', {as='treesitter', ['do'] = 'TSUpdate'})
+Plug('lewis6991/gitsigns.nvim', {as='gitsigns'})
+Plug('antoinemadec/FixCursorHold.nvim', {as='cursorhold'})
+Plug('folke/trouble.nvim', {as='trouble'})
+Plug('ahmedkhalf/project.nvim', {as='project_nvim'})
+Plug('terrortylor/nvim-comment',  {as='nvim_comment'})
+Plug('hrsh7th/vim-vsnip')
+Plug('hrsh7th/vim-vsnip-integ')
+
+--Plug('', {as=''})
+
+vim.call('plug#end')
+
+
 -- changing defaults
 vim.notify = require("notify")
 
@@ -14,7 +47,7 @@ require('commands')
 require('plugin-configs.tree')
 require('plugin-configs.compe')
 require('plugin-configs.dashboard')
-require('plugin-configs.bufferline')
+--require('plugin-configs.bufferline') -- issue with notify
 require('plugin-configs.neogit')
 require('plugin-configs.impatient')
 require('plugin-configs.lspconfig')
--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,4 +1,4 @@
 -- set standard colorscheme
-vim.cmd("colorscheme everforest")
+vim.cmd("colorscheme dracula")
 -- set standard light/dark mode
-vim.opt.background = "dark"
+--vim.opt.background = "dark"
